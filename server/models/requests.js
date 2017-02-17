4ed94f35431f21d5ae22f5a4f127ee00878ab515
KhaoulaZitoun <khaoula.zitoun@gmail.com>
2017-02-17 17:32:19 +0100

    add configuration for cozydb

--- server/models/requests.js
+++ server/models/requests.js
@@ -4,6 +4,7 @@ var cozydb = require('cozydb');
 // Toutes les vues de la base de données à ajouter ici
 module.exports = {
     client: {
-        all: cozydb.defaultRequests.all
+        all: cozydb.defaultRequests.all,
+        byCreditor: cozydb.defaultRequests.by('creditor')
     }
 };
